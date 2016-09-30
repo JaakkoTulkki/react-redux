7217902e1aab0d0d299301b242f3fc5afac523b9
Jaakko Tulkki <jaakko.tulkki@sainsburys.co.uk>
2016-09-30 16:26:04 +0100

    added stuff

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return(
+            <div>
+                <hi>About</hi>
+                <p>this application uses this and that</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
